e880b224574e05e50eea57dbcaef137f485ddb14
gong-yeongbin <youngbin816@gmail.com>
2024-01-04 19:45:35 +0900

    fix(codedeploy): logs file delete
    logs file delete error

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/kisuj
+REPOSITORY=/home/ubuntu
 cd $REPOSITORY
 
 echo '========================================'
 echo 'delete pm2 all'
 echo '========================================'
 pm2 delete all
+
+REPOSITORY=/home/ubuntu/kisuj
+cd $REPOSITORY
 
 echo '========================================'
 echo 'delete src directory'
